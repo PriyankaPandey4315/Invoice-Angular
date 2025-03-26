3e9f78b7d15650cdf6cc536320e7eb0ee96b5a35
PriyankaPandey4315 <priyank.4851115@gmail.com>
2025-03-26 17:20:21 +0530

    changes in angular.json file

--- src/app/app.modules.ts
+++ src/app/app.modules.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FullCalendarModule } from '@fullcalendar/angular';
-import { QuillModule } from 'ngx-quill';
 
 
 @NgModule({
@@ -11,7 +10,6 @@ import { QuillModule } from 'ngx-quill';
     FullCalendarModule,
     AppComponent, // ✅ Import standalone component directly
     BrowserModule,
-    QuillModule
   ],
   exports: []
 })
